a7d5ae04d075889685543d198853f6f0d5114342
haoxiaofan <569192072@qq.com>
2018-04-20 22:17:23 +0800

    commit modify

--- NewCrmCore.Dto/Settings.cs
+++ NewCrmCore.Dto/Settings.cs
@@ -6,6 +6,32 @@ namespace NewCrmCore.Dto
 {
 	public class Settings
 	{
-		
+		public Database Database { get; set; }
+
+		public String Skin { get; set; }
+
+		public String FileUrl { get; set; }
+
+		public Redis Redis { get; set; }
+
+		public String Mongodb { get; set; }
+
+		public String UploadUrl { get; set; }
+	}
+
+	public class Database
+	{
+		public String Name { get; set; }
+
+		public String Value { get; set; }
+	}
+
+	public class Redis
+	{
+		public Int32 DatabaseIndex { get; set; }
+
+		public String Connection { get; set; }
+
+		public String Prefix { get; set; }
 	}
 }
