b7ce2449cc5f4af11944e41899ca914b85debe2e
haoxiaofan <569192072@qq.com>
2018-05-14 11:41:59 +0800

    w

--- NewCrmCore.Domain.Services/BoundedContext/AccountContext.cs
+++ NewCrmCore.Domain.Services/BoundedContext/AccountContext.cs
@@ -90,7 +90,7 @@ namespace NewCrmCore.Domain.Services.BoundedContext
 			 });
 		}
 
-		public async Task<Config> GetConfigAsync(Int32 accountId)
+		public async Task<Config> GetConfigAsync(Int32 accountId) 
 		{
 			new Parameter().Validate(accountId);
 			return await Task.Run(() => 
