42877c6e3654aa2ab9bdebb1afdde36fca0887a1
haoxiaofan <569192072@qq.com>
2018-05-20 12:21:38 +0800

    w

--- NewCrmCore.Application.Services/AppServices.cs
+++ NewCrmCore.Application.Services/AppServices.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using NewCrmCore.Application.Services.Interface;
 using NewCrmCore.Domain.Entitys.System;
-using NewCrmCore.Domain.Services;
 using NewCrmCore.Domain.Services.Interface;
 using NewCrmCore.Domain.ValueObject;
 using NewCrmCore.Dto;
--- NewCrmCore.Infrastructure/CommonTools/CustomExtension/Extension.cs
+++ NewCrmCore.Infrastructure/CommonTools/CustomExtension/Extension.cs
@@ -4,11 +4,6 @@ namespace NewCrmCore.Infrastructure.CommonTools.CustomExtension
 {
 	public static class Extension
 	{
-		public static Int32 ToInt32(this Boolean boo)
-		{
-			return boo ? 1 : 0;
-		}
-
 		public static Int32 ToInt32(this Enum e)
 		{
 			return (Int32)Enum.Parse(e.GetType(), e.ToString());
