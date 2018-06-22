7e20c6aa1969d7ae6f15e7ac18c3dc15380e3e0c
zifan.hao <569192072@qq.com>
2018-06-22 14:12:58 +0800

    w

--- NewCrmCore.Web/Controllers/AppManagerController.cs
+++ NewCrmCore.Web/Controllers/AppManagerController.cs
@@ -10,12 +10,12 @@ using NewLibCore.Validate;
 
 namespace NewCrmCore.Web.Controllers
 {
-	public class AppManagerController: BaseController
+	public class AppManagerController : BaseController
 	{
 		private readonly IAppServices _appServices;
 		private readonly IAccountServices _accountServices;
 
-		public AppManagerController(IAppServices appServices,IAccountServices accountServices)
+		public AppManagerController(IAppServices appServices, IAccountServices accountServices)
 		{
 			_appServices = appServices;
 			_accountServices = accountServices;
@@ -56,29 +56,10 @@ namespace NewCrmCore.Web.Controllers
 
 		#endregion
 
-		/// <summary>
-		/// 获取所有的app
-		/// </summary>
-		[HttpGet]
-		public async Task<ActionResult> GetApps(String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
-		{
-			var response = new ResponseModels<IList<AppDto>>();
-			var result = await _appServices.GetAccountAppsAsync(0, searchText, appTypeId, appStyleId, appState, pageIndex, pageSize);
-			foreach (var appDto in result.Models)
-			{
-				appDto.IsCreater = appDto.AccountId == AccountId;
-			}
-
-			response.TotalCount = result.TotalCount;
-			response.IsSuccess = true;
-			response.Message = "获取app列表成功";
-			response.Model = result.Models;
-
-			return Json(response);
-		}
+		#region 审核通过
 
 		/// <summary>
-		/// app审核通过
+		/// 审核通过
 		/// </summary>
 		[HttpPost]
 		public async Task<ActionResult> Pass(Int32 appId)
@@ -95,41 +76,9 @@ namespace NewCrmCore.Web.Controllers
 			return Json(response);
 		}
 
-		/// <summary>
-		/// app审核不通过
-		/// </summary>
-		[HttpPost]
-		public async Task<ActionResult> Deny(Int32 appId)
-		{
-			#region 参数验证	
-			new Parameter().Validate(appId);
-			#endregion
+		#endregion
 
-			var response = new ResponseModel();
-			await _appServices.DenyAsync(appId);
-			response.IsSuccess = true;
-			response.Message = "app审核不通过";
-
-			return Json(response);
-		}
-
-		/// <summary>
-		/// 设置app为今日推荐
-		/// </summary>
-		[HttpPost]
-		public async Task<ActionResult> Recommend(Int32 appId)
-		{
-			#region 参数验证	
-			new Parameter().Validate(appId);
-			#endregion
-
-			var response = new ResponseModel();
-			await _appServices.SetTodayRecommandAppAsync(appId);
-			response.IsSuccess = true;
-			response.Message = "设置成功";
-
-			return Json(response);
-		}
+		#region 移除应用
 
 		/// <summary>
 		/// 删除app
@@ -149,6 +98,56 @@ namespace NewCrmCore.Web.Controllers
 			return Json(response);
 		}
 
+		#endregion
+
+		#region 审核不通过
+
+		/// <summary>
+		/// 审核不通过
+		/// </summary>
+		[HttpPost]
+		public async Task<ActionResult> Deny(Int32 appId)
+		{
+			#region 参数验证	
+			new Parameter().Validate(appId);
+			#endregion
+
+			var response = new ResponseModel();
+			await _appServices.DenyAsync(appId);
+			response.IsSuccess = true;
+			response.Message = "app审核不通过";
+
+			return Json(response);
+		}
+
+		#endregion
+
+		#region 获取所有应用
+
+		/// <summary>
+		/// 获取所有应用
+		/// </summary>
+		[HttpGet]
+		public async Task<ActionResult> GetApps(String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
+		{
+			var response = new ResponseModels<IList<AppDto>>();
+			var result = await _appServices.GetAccountAppsAsync(0, searchText, appTypeId, appStyleId, appState, pageIndex, pageSize);
+			foreach (var appDto in result.Models)
+			{
+				appDto.IsCreater = appDto.AccountId == AccountId;
+			}
+
+			response.TotalCount = result.TotalCount;
+			response.IsSuccess = true;
+			response.Message = "获取app列表成功";
+			response.Model = result.Models;
+
+			return Json(response);
+		}
+
+		#endregion
+
+		#region 检查应用名称
 
 		/// <summary>
 		/// 检查应用名称
@@ -164,6 +163,10 @@ namespace NewCrmCore.Web.Controllers
 			return Json(!result ? new { status = "y", info = "" } : new { status = "n", info = "应用名称已存在" });
 		}
 
+		#endregion
+
+		#region 检查应用地址
+
 		/// <summary>
 		/// 检查应用Url
 		/// </summary>
@@ -177,5 +180,29 @@ namespace NewCrmCore.Web.Controllers
 			var result = await _accountServices.CheckAppUrlAsync(param);
 			return Json(!result ? new { status = "y", info = "" } : new { status = "n", info = "应用Url已存在" });
 		}
+
+		#endregion
+
+		#region 设置今日推荐应用
+
+		/// <summary>
+		/// 设置应用为今日推荐
+		/// </summary>
+		[HttpPost]
+		public async Task<ActionResult> Recommend(Int32 appId)
+		{
+			#region 参数验证	
+			new Parameter().Validate(appId);
+			#endregion
+
+			var response = new ResponseModel();
+			await _appServices.SetTodayRecommandAppAsync(appId);
+			response.IsSuccess = true;
+			response.Message = "设置成功";
+
+			return Json(response);
+		}
+
+		#endregion
 	}
 }
