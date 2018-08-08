e1c400dabd9c427c1c140e7570486f94282cf869
zifan.hao <569192072@qq.com>
2018-08-08 16:28:02 +0800

    w

--- NewCrmCore.Web/Controllers/AccountManagerController.cs
+++ NewCrmCore.Web/Controllers/AccountManagerController.cs
@@ -13,213 +13,215 @@ using static NewCrmCore.Infrastructure.CommonTools.CacheKey;
 
 namespace NewCrmCore.Web.Controllers
 {
-	public class AccountManagerController : BaseController
-	{
-		private readonly ISecurityServices _securityServices;
-		private readonly IAccountServices _accountServices;
+    public class AccountManagerController : BaseController
+    {
+        private readonly ISecurityServices _securityServices;
+        private readonly IAccountServices _accountServices;
 
-		public AccountManagerController(ISecurityServices securityServices, IAccountServices accountServices)
-		{
-			_securityServices = securityServices;
-			_accountServices = accountServices;
-		}
+        public AccountManagerController(ISecurityServices securityServices, IAccountServices accountServices)
+        {
+            _securityServices = securityServices;
+            _accountServices = accountServices;
+        }
 
-		#region 页面
+        #region 页面
 
-		/// <summary>
-		/// 首页
-		/// </summary>
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return View();
-		}
+        /// <summary>
+        /// 首页
+        /// </summary>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		/// <summary>
-		/// 创建新账户
-		/// </summary>
-		[HttpGet]
-		public async Task<IActionResult> Account(Int32 accountId = 0)
-		{
-			if (accountId != 0)
-			{
-				ViewData["Account"] = await _accountServices.GetAccountAsync(accountId);
-			}
-			ViewData["Roles"] = (await _securityServices.GetRolesAsync("", 1, 100)).Models;
-			return View();
-		}
+        /// <summary>
+        /// 创建新账户
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Account(Int32 accountId = 0)
+        {
+            if (accountId != 0)
+            {
+                ViewData["Account"] = await _accountServices.GetAccountAsync(accountId);
+            }
+            ViewData["Roles"] = (await _securityServices.GetRolesAsync("", 1, 100)).Models;
+            return View();
+        }
 
-		#endregion
+        #endregion
 
-		#region 移除账户
+        #region 移除账户
 
-		/// <summary>
-		/// 移除账户
-		/// </summary>
-		[HttpPost]
-		public async Task<IActionResult> RemoveAccount(Int32 accountId)
-		{
-			#region 参数验证
-			new Parameter().Validate(accountId);
-			#endregion
+        /// <summary>
+        /// 移除账户
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> RemoveAccount(Int32 accountId)
+        {
+            await Task.Delay(5000);
 
-			var response = new ResponseModel<String>();
-			await _accountServices.RemoveAccountAsync(accountId);
-			response.IsSuccess = true;
-			response.Message = "移除账户成功";
+            #region 参数验证
+            new Parameter().Validate(accountId);
+            #endregion
 
-			return Json(response);
-		}
+            var response = new ResponseModel<String>();
+            await _accountServices.RemoveAccountAsync(accountId);
+            response.IsSuccess = true;
+            response.Message = "移除账户成功";
 
-		#endregion
+            return Json(response);
+        }
 
-		#region 账户启用
+        #endregion
 
-		/// <summary>
-		/// 账户启用
-		/// </summary>
-		[HttpPost]
-		public async Task<IActionResult> Enable(Int32 accountId)
-		{
-			#region 参数验证
-			new Parameter().Validate(accountId);
-			#endregion
+        #region 账户启用
 
-			var response = new ResponseModel<String>();
-			await _accountServices.EnableAsync(accountId);
-			response.IsSuccess = true;
-			response.Message = "启用账户成功";
+        /// <summary>
+        /// 账户启用
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Enable(Int32 accountId)
+        {
+            #region 参数验证
+            new Parameter().Validate(accountId);
+            #endregion
 
-			return Json(response);
-		}
+            var response = new ResponseModel<String>();
+            await _accountServices.EnableAsync(accountId);
+            response.IsSuccess = true;
+            response.Message = "启用账户成功";
 
-		#endregion
+            return Json(response);
+        }
 
-		#region 账户禁用
+        #endregion
 
-		/// <summary>
-		/// 账户禁用
-		/// </summary>
-		[HttpPost]
-		public async Task<IActionResult> Disable(Int32 accountId)
-		{
-			#region 参数验证
-			new Parameter().Validate(accountId);
-			#endregion
+        #region 账户禁用
 
-			var response = new ResponseModel<String>();
-			await _accountServices.DisableAsync(accountId);
-			response.IsSuccess = true;
-			response.Message = "禁用账户成功";
+        /// <summary>
+        /// 账户禁用
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Disable(Int32 accountId)
+        {
+            #region 参数验证
+            new Parameter().Validate(accountId);
+            #endregion
 
-			return Json(response);
-		}
+            var response = new ResponseModel<String>();
+            await _accountServices.DisableAsync(accountId);
+            response.IsSuccess = true;
+            response.Message = "禁用账户成功";
 
-		#endregion
+            return Json(response);
+        }
 
-		#region 创建新账户
+        #endregion
 
-		/// <summary>
-		/// 创建新账户
-		/// </summary>
-		[HttpPost]
-		public async Task<IActionResult> CreateAccount(IFormCollection forms)
-		{
-			#region 参数验证
-			new Parameter().Validate(forms);
-			#endregion
+        #region 创建新账户
 
-			var response = new ResponseModel<AccountDto>();
-			var dto = WapperAccountDto(forms);
-			if (dto.Id == 0)
-			{
-				await _accountServices.AddNewAccountAsync(dto);
+        /// <summary>
+        /// 创建新账户
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount(IFormCollection forms)
+        {
+            #region 参数验证
+            new Parameter().Validate(forms);
+            #endregion
 
-				response.Message = "创建新账户成功";
-				response.IsSuccess = true;
-			}
-			else
-			{
-				await _accountServices.ModifyAccountAsync(dto);
-				if (!String.IsNullOrEmpty(dto.Password))
-				{
-					Response.Cookies.Append("Account", AccountId.ToString(), new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
-				}
-				response.Message = "修改账户成功";
-				response.IsSuccess = true;
-			}
+            var response = new ResponseModel<AccountDto>();
+            var dto = WapperAccountDto(forms);
+            if (dto.Id == 0)
+            {
+                await _accountServices.AddNewAccountAsync(dto);
+
+                response.Message = "创建新账户成功";
+                response.IsSuccess = true;
+            }
+            else
+            {
+                await _accountServices.ModifyAccountAsync(dto);
+                if (!String.IsNullOrEmpty(dto.Password))
+                {
+                    Response.Cookies.Append("Account", AccountId.ToString(), new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
+                }
+                response.Message = "修改账户成功";
+                response.IsSuccess = true;
+            }
 
 
-			return Json(response);
-		}
+            return Json(response);
+        }
 
-		#endregion
+        #endregion
 
-		#region 获取所有账户
+        #region 获取所有账户
 
-		/// <summary>
-		/// 获取所有账户
-		/// </summary>
-		[HttpGet]
-		public async Task<IActionResult> Accounts(String accountName, String accountType, Int32 pageIndex, Int32 pageSize)
-		{
-			var response = new ResponseModels<IList<AccountDto>>();
+        /// <summary>
+        /// 获取所有账户
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Accounts(String accountName, String accountType, Int32 pageIndex, Int32 pageSize)
+        {
+            var response = new ResponseModels<IList<AccountDto>>();
 
-			var result = await _accountServices.GetAccountsAsync(accountName, accountType, pageIndex, pageSize);
-			if (result != null)
-			{
-				response.TotalCount = result.TotalCount;
-				response.Message = "获取账户列表成功";
-				response.Model = result.Models;
-				response.IsSuccess = true;
-			}
+            var result = await _accountServices.GetAccountsAsync(accountName, accountType, pageIndex, pageSize);
+            if (result != null)
+            {
+                response.TotalCount = result.TotalCount;
+                response.Message = "获取账户列表成功";
+                response.Model = result.Models;
+                response.IsSuccess = true;
+            }
 
-			return Json(response);
-		}
+            return Json(response);
+        }
 
-		#endregion
+        #endregion
 
-		#region 检查账户名是否已经存在
+        #region 检查账户名是否已经存在
 
-		/// <summary>
-		/// 检查账户名是否已经存在
-		/// </summary>
-		[HttpPost]
-		public async Task<IActionResult> CheckName(String param)
-		{
-			#region 参数验证
-			new Parameter().Validate(param);
-			#endregion
+        /// <summary>
+        /// 检查账户名是否已经存在
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> CheckName(String param)
+        {
+            #region 参数验证
+            new Parameter().Validate(param);
+            #endregion
 
-			var result = await _accountServices.CheckAccountNameExistAsync(param);
-			return Json(result ? new { status = "y", info = "" } : new { status = "n", info = "用户名已存在" });
-		}
+            var result = await _accountServices.CheckAccountNameExistAsync(param);
+            return Json(result ? new { status = "y", info = "" } : new { status = "n", info = "用户名已存在" });
+        }
 
-		#endregion
+        #endregion
 
-		#region private method
+        #region private method
 
-		private AccountDto WapperAccountDto(IFormCollection forms)
-		{
-			var roleIds = new List<RoleDto>();
-			if ((forms["val_roleIds"] + "").Length > 0)
-			{
-				roleIds = forms["val_roleIds"].ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(role => new RoleDto
-				{
-					Id = Int32.Parse(role)
-				}).ToList();
-			}
+        private AccountDto WapperAccountDto(IFormCollection forms)
+        {
+            var roleIds = new List<RoleDto>();
+            if ((forms["val_roleIds"] + "").Length > 0)
+            {
+                roleIds = forms["val_roleIds"].ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(role => new RoleDto
+                {
+                    Id = Int32.Parse(role)
+                }).ToList();
+            }
 
-			return new AccountDto
-			{
-				Id = String.IsNullOrEmpty(forms["id"]) ? 0 : Int32.Parse(forms["id"]),
-				Name = forms["val_accountname"],
-				Password = forms["val_password"],
-				IsAdmin = Int32.Parse(forms["val_type"]) == 2,
-				Roles = roleIds
-			};
-		}
+            return new AccountDto
+            {
+                Id = String.IsNullOrEmpty(forms["id"]) ? 0 : Int32.Parse(forms["id"]),
+                Name = forms["val_accountname"],
+                Password = forms["val_password"],
+                IsAdmin = Int32.Parse(forms["val_type"]) == 2,
+                Roles = roleIds
+            };
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
