c4a8c03042dabc0d58b981e5053f1d21985984d2
zifan.hao <569192072@qq.com>
2018-09-04 16:17:10 +0800

    修改注释

--- NewCrmCore.Application.Services/Interface/ILoggerServices.cs
+++ NewCrmCore.Application.Services/Interface/ILoggerServices.cs
@@ -5,10 +5,23 @@ using NewCrmCore.Infrastructure.CommonTools;
 
 namespace NewCrmCore.Application.Services.Interface
 {
-	public interface ILoggerServices
-	{
-		Task AddLoggerAsync(LogDto log);
+    public interface ILoggerServices
+    {
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        Task AddLoggerAsync(LogDto log);
 
-		Task<PageList<LogDto>> GetLogsAsync(Int32 accountId, Int32 logLevel, Int32 pageIndex, Int32 pageSize);
-	}
+        /// <summary>
+        /// 获取日志
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="logLevel"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        Task<PageList<LogDto>> GetLogsAsync(Int32 accountId, Int32 logLevel, Int32 pageIndex, Int32 pageSize);
+    }
 }
--- NewCrmCore.Application.Services/Interface/ISecurityServices.cs
+++ NewCrmCore.Application.Services/Interface/ISecurityServices.cs
@@ -6,59 +6,81 @@ using NewCrmCore.Infrastructure.CommonTools;
 
 namespace NewCrmCore.Application.Services.Interface
 {
-	public interface ISecurityServices
-	{
-		#region have return value
+    public interface ISecurityServices
+    {
+        #region have return value
 
-		/// <summary>
-		/// 获取全部的角色
-		/// </summary>
-		Task<PageList<RoleDto>> GetRolesAsync(String roleName, Int32 pageIndex, Int32 pageSize);
+        /// <summary>
+        /// 获取全部的角色
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        Task<PageList<RoleDto>> GetRolesAsync(String roleName, Int32 pageIndex, Int32 pageSize);
 
-		/// <summary>
-		/// 根据角色Id获取角色
-		/// </summary>
-		Task<RoleDto> GetRoleAsync(Int32 roleId);
+        /// <summary>
+        /// 根据角色Id获取角色
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        Task<RoleDto> GetRoleAsync(Int32 roleId);
 
-		/// <summary>
-		/// 检查用户权限
-		/// </summary>
-		Task<Boolean> CheckPermissionsAsync(Int32 accessAppId, params Int32[] roleIds);
+        /// <summary>
+        /// 检查用户权限
+        /// </summary>
+        /// <param name="accessAppId"></param>
+        /// <param name="roleIds"></param>
+        /// <returns></returns>
+        Task<Boolean> CheckPermissionsAsync(Int32 accessAppId, params Int32[] roleIds);
 
-		/// <summary>
-		/// 检查角色名称
-		/// </summary>
-		Task<Boolean> CheckRoleNameAsync(String name);
+        /// <summary>
+        /// 检查角色名称
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<Boolean> CheckRoleNameAsync(String name);
 
-		/// <summary>
-		/// 检查角色标识
-		/// </summary>
-		Task<Boolean> CheckRoleIdentityAsync(String name);
+        /// <summary>
+        /// 检查角色标识
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<Boolean> CheckRoleIdentityAsync(String name);
 
-		#endregion
+        #endregion
 
-		#region not have return value
+        #region not have return value
 
-		/// <summary>
-		/// 新建角色
-		/// </summary>
-		Task AddNewRoleAsync(RoleDto role);
+        /// <summary>
+        /// 新建角色
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task AddNewRoleAsync(RoleDto role);
 
-		/// <summary>
-		/// 修改角色信息
-		/// </summary>
-		Task ModifyRoleAsync(RoleDto role);
+        /// <summary>
+        /// 修改角色信息
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task ModifyRoleAsync(RoleDto role);
 
-		/// <summary>
-		/// 添加权限到当前的角色
-		/// </summary>
-		Task AddPowerToCurrentRoleAsync(Int32 roleId, IEnumerable<Int32> powerIds);
+        /// <summary>
+        /// 添加权限到当前的角色
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <param name="powerIds"></param>
+        /// <returns></returns>
+        Task AddPowerToCurrentRoleAsync(Int32 roleId, IEnumerable<Int32> powerIds);
 
-		/// <summary>
-		/// 移除角色
-		/// </summary>
-		Task RemoveRoleAsync(Int32 roleId);
+        /// <summary>
+        /// 移除角色
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        Task RemoveRoleAsync(Int32 roleId);
 
-		#endregion
-	}
+        #endregion
+    }
 }
