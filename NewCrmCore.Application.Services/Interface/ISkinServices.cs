2fecdfd6dc53df6690b24409db2aac103fb6e8bf
zifan.hao <569192072@qq.com>
2018-09-04 16:19:47 +0800

    修改注释

--- NewCrmCore.Application.Services/Interface/ISkinServices.cs
+++ NewCrmCore.Application.Services/Interface/ISkinServices.cs
@@ -4,20 +4,20 @@ using System.Threading.Tasks;
 
 namespace NewCrmCore.Application.Services.Interface
 {
-	public interface ISkinServices
-	{
-		/// <summary>
-		/// 获取全部的皮肤
-		/// </summary>
-		/// <param name="skinPath"></param>
-		/// <returns></returns>
-		Task<IDictionary<String, dynamic>> GetAllSkinAsync(String skinPath);
+    public interface ISkinServices
+    {
+        /// <summary>
+        /// 获取全部的皮肤
+        /// </summary>
+        /// <param name="skinPath"></param>
+        /// <returns></returns>
+        Task<IDictionary<String, dynamic>> GetAllSkinAsync(String skinPath);
 
-		/// <summary>
-		/// 修改默认显示的皮肤
-		/// </summary>
-		/// <param name="accountId"></param>
-		/// <param name="newSkin"></param>
-		Task ModifySkinAsync(Int32 accountId, String newSkin);
-	}
+        /// <summary>
+        /// 修改默认显示的皮肤
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <param name="newSkin"></param>
+        Task ModifySkinAsync(Int32 accountId, String newSkin);
+    }
 }
