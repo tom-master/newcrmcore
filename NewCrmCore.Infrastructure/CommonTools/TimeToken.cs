ed1739ca94b189c232d9ce935d60b286fe1eb09a
zifan.hao <569192072@qq.com>
2018-09-13 11:45:55 +0800

    组织代码

--- NewCrmCore.Infrastructure/CommonTools/TimeToken.cs
+++ NewCrmCore.Infrastructure/CommonTools/TimeToken.cs
@@ -19,9 +19,7 @@ namespace NewCrmCore.Infrastructure.CommonTools
                 DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
                 DateTime dtNow = DateTime.Now;
                 Int32 seconds = Convert.ToInt32(dtNow.Subtract(dt).TotalSeconds);
-
                 var hashCode = $@"{Guid.NewGuid()}{seconds}{new Random(seconds).Next(999999999)}".GetHashCode();
-
                 return Convert.ToBase64String(Encoding.UTF8.GetBytes(hashCode.ToString()));
             });
 
