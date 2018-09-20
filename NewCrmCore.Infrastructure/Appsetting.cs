6e78dbf6acf5d767de209a5968167f537f10e5ad
haoxiaofan <569192072@qq.com>
2018-09-20 08:04:01 +0000

    修改获取不到环境变量时不抛出具体异常的问题

--- NewCrmCore.Infrastructure/Appsetting.cs
+++ NewCrmCore.Infrastructure/Appsetting.cs
@@ -3,69 +3,119 @@ using NewLibCore;
 
 namespace NewCrmCore.Infrastructure
 {
-	public class Appsetting
-	{
-		public static String Database
-		{
-			get
-			{
-				var str =Host.GetHostVar("NewCrmDatabase");
-				return str ?? "";
-			}
-		}
+    public class Appsetting
+    {
+        public static String Database
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmDatabase");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
 
-		public static String Redis
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmRedis");
-				return str ?? "";
-			}
-		}
+                    throw;
+                }
+            }
+        }
 
-		public static String Mongodb
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmMongodb");
-				return str ?? "";
-			}
-		}
+        public static String Redis
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmRedis");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
 
-		public static String UploadUrl
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmFileUploadUrl");
-				return str ?? "";
-			}
-		}
+        public static String Mongodb
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmMongodb");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
 
-		public static String FileUrl
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmFileUrl");
-				return str ?? "";
-			}
-		}
+        public static String UploadUrl
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmFileUploadUrl");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
 
-		public static String FileStorage
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmFileStorage");
-				return str ?? "";
-			}
-		}
+        public static String FileUrl
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmFileUrl");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
 
-		public static String Skin
-		{
-			get
-			{
-				var str = Host.GetHostVar("NewCrmSkin");
-				return str ?? "";
-			}
-		}
-	}
+        public static String FileStorage
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmFileStorage");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static String Skin
+        {
+            get
+            {
+                try
+                {
+                    var str = Host.GetHostVar("NewCrmSkin");
+                    return str ?? "";
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
 }
