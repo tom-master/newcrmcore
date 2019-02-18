3859d389d125e12adb84218aaaece8f774e8b38f
haoxiaofan <569192072@qq.com>
2019-02-18 09:57:46 +0800

    格式化代码

--- NewCrmCore.FileServices/Controllers/FileController.cs
+++ NewCrmCore.FileServices/Controllers/FileController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NewCrmCore.Infrastructure;
-using System;
-using System.Collections.Generic;
+﻿using NewCrmCore.Infrastructure;
 
 namespace NewCrmCore.FileServices.Controllers
 {
@@ -23,49 +20,29 @@ namespace NewCrmCore.FileServices.Controllers
 
                 if (String.IsNullOrEmpty(userId))
                 {
-                    responses.Add(new UploadResponseModel
-                    {
-                        IsSuccess = false,
-                        Message = $@"{nameof(userId)}参数验证失败"
-                    });
-
+                    responses.Add(new UploadResponseModel { IsSuccess = false, Message = $@"{nameof(userId)}参数验证失败" });
                     return Json(responses);
                 }
 
                 var files = request.Files;
                 if (files.Count == 0)
                 {
-                    responses.Add(new UploadResponseModel
-                    {
-                        IsSuccess = false,
-                        Message = "未能获取到上传的文件"
-                    });
-
+                    responses.Add(new UploadResponseModel { IsSuccess = false, Message = "未能获取到上传的文件" });
                     return Json(responses);
                 }
 
                 var uploadType = request["uploadtype"];
                 if (String.IsNullOrEmpty(uploadType))
                 {
-                    responses.Add(new UploadResponseModel
-                    {
-                        IsSuccess = false,
-                        Message = $@"{nameof(uploadType)}参数验证失败"
-                    });
-
+                    responses.Add(new UploadResponseModel { IsSuccess = false, Message = $@"{nameof(uploadType)}参数验证失败" });
                     return Json(responses);
                 }
 
                 if (String.IsNullOrEmpty(Appsetting.FileStorage))
                 {
-                    responses.Add(new UploadResponseModel
-                    {
-                        IsSuccess = false,
-                        Message = $@"未能找到服务器配置的存储路径"
-                    });
+                    responses.Add(new UploadResponseModel { IsSuccess = false, Message = $@"未能找到服务器配置的存储路径" });
                     return Json(responses);
                 }
-
 
                 for (var i = 0; i < files.Count; i++)
                 {
@@ -75,24 +52,14 @@ namespace NewCrmCore.FileServices.Controllers
                     var fileExtension = requestUpload.CheckFileExtension(file);
                     if (String.IsNullOrEmpty(fileExtension))
                     {
-                        responses.Add(new UploadResponseModel
-                        {
-                            IsSuccess = false,
-                            Message = $@"后缀名为：{fileExtension}的文件禁止上传"
-                        });
-
+                        responses.Add(new UploadResponseModel { IsSuccess = false, Message = $@"后缀名为：{fileExtension}的文件禁止上传" });
                         continue;
                     }
 
                     var requestFile = requestUpload.InitRequiredParameters(userId, fileExtension);
                     if (!requestFile.CreateFile(file))
                     {
-                        responses.Add(new UploadResponseModel
-                        {
-                            IsSuccess = false,
-                            Message = $@"文件上传失败"
-                        });
-
+                        responses.Add(new UploadResponseModel { IsSuccess = false, Message = $@"文件上传失败" });
                         continue;
                     }
 
@@ -101,11 +68,7 @@ namespace NewCrmCore.FileServices.Controllers
             }
             catch (Exception ex)
             {
-                responses.Add(new UploadResponseModel
-                {
-                    IsSuccess = false,
-                    Message = ex.ToString()
-                });
+                responses.Add(new UploadResponseModel { IsSuccess = false, Message = ex.ToString() });
             }
 
             return Json(responses);
