a69493815f888cd2a8ab10f1e02ee0cefac8fc8d
haoxiaofan <569192072@qq.com>
2019-05-04 20:26:18 +0800

    add commit

--- NewCrmCore.Domain.Services/BoundedContext/SecurityContext.cs
+++ NewCrmCore.Domain.Services/BoundedContext/SecurityContext.cs
@@ -7,325 +7,321 @@ using NewCrmCore.Domain.Entitys.Agent;
 using NewCrmCore.Domain.Entitys.Security;
 using NewCrmCore.Domain.Entitys.System;
 using NewCrmCore.Domain.Services.Interface;
-using NewCrmCore.Infrastructure;
 using NewCrmCore.Infrastructure.CommonTools;
 using NewLibCore.Data.SQL.Mapper;
 using NewLibCore.Validate;
 
 namespace NewCrmCore.Domain.Services.BoundedContext
 {
-    public class SecurityContext : ISecurityContext
-    {
-        public async Task<List<RolePower>> GetPowersAsync()
-        {
-            return await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    return mapper.Find<RolePower>(a => new
-                    {
-                        a.RoleId,
-                        a.AppId
-                    }).ToList();
+	public class SecurityContext : ISecurityContext
+	{
+		public async Task<List<RolePower>> GetPowersAsync()
+		{
+			return await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					return mapper.Select<RolePower>(a => new
+					{
+						a.RoleId,
+						a.AppId
+					}).ToList();
 
-                    //var sql = $@"SELECT a.RoleId, a.AppId FROM RolePower AS a WHERE a.IsDeleted=0";
-                    //return mapper.Find<RolePower>(sql);
-                }
-            });
-        }
+					//var sql = $@"SELECT a.RoleId, a.AppId FROM RolePower AS a WHERE a.IsDeleted=0";
+					//return mapper.Find<RolePower>(sql);
+				}
+			});
+		}
 
-        public async Task<Role> GetRoleAsync(Int32 roleId)
-        {
-            Parameter.Validate(roleId);
+		public async Task<Role> GetRoleAsync(Int32 roleId)
+		{
+			Parameter.Validate(roleId);
 
-            return await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    return mapper.Find<Role>(a => a.Id == roleId, a => new { a.Id, a.Name, a.RoleIdentity }).FirstOrDefault();
-                    //var sql = $@"SELECT a.Id, a.Name, a.RoleIdentity, a.Remark FROM Role AS a WHERE a.Id=@Id AND a.IsDeleted=0";
-                    //var parameters = new List<EntityParameter> { new EntityParameter("@Id", roleId) };
-                    //return mapper.FindOne<Role>(sql, parameters);
-                }
-            });
-        }
+			return await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					return mapper.Select<Role>(a => new { a.Id, a.Name, a.RoleIdentity }).Where(a => a.Id == roleId).ToOne();
+					//var sql = $@"SELECT a.Id, a.Name, a.RoleIdentity, a.Remark FROM Role AS a WHERE a.Id=@Id AND a.IsDeleted=0";
+					//var parameters = new List<EntityParameter> { new EntityParameter("@Id", roleId) };
+					//return mapper.FindOne<Role>(sql, parameters);
+				}
+			});
+		}
 
-        public List<Role> GetRoles(String roleName, Int32 pageIndex, Int32 pageSize, out Int32 totalCount)
-        {
-            using (var mapper = new EntityMapper())
-            {
-                var where = new StringBuilder();
-                var parameters = new List<EntityParameter>();
-                if (!String.IsNullOrEmpty(roleName))
-                {
-                    parameters.Add(new EntityParameter("@roleName", $@"%{roleName}%"));
-                    where.Append($@" AND a.Name LIKE @roleName");
-                }
+		public List<Role> GetRoles(String roleName, Int32 pageIndex, Int32 pageSize, out Int32 totalCount)
+		{
+			using (var mapper = new EntityMapper())
+			{
+				var where = new StringBuilder();
+				var parameters = new List<EntityParameter>();
+				if (!String.IsNullOrEmpty(roleName))
+				{
+					parameters.Add(new EntityParameter("@roleName", $@"%{roleName}%"));
+					where.Append($@" AND a.Name LIKE @roleName");
+				}
 
-                #region totalCount
-                {
-                    var sql = $@"SELECT  COUNT(*) FROM Role AS a WHERE 1=1 {where} AND a.IsDeleted=0";
-                    totalCount = mapper.ComplexSqlExecute<Int32>(sql, parameters);
-                }
-                #endregion
+				#region totalCount
+				{
+					var sql = $@"SELECT  COUNT(*) FROM Role AS a WHERE 1=1 {where} AND a.IsDeleted=0";
+					totalCount = mapper.ExecuteSql<Int32>(sql, parameters);
+				}
+				#endregion
 
-                #region sql
-                {
-                    var sql = $@"SELECT
+				#region sql
+				{
+					var sql = $@"SELECT
 								a.Name,
 								a.RoleIdentity,
 								a.Remark,
 								a.Id
 								FROM Role AS a WHERE 1=1 {where} AND a.IsDeleted=0 LIMIT {pageSize * (pageIndex - 1)},{pageSize}";
-                    return mapper.ComplexSqlExecute<List<Role>>(sql, parameters);
-                }
-                #endregion
+					return mapper.ExecuteSql<List<Role>>(sql, parameters);
+				}
+				#endregion
 
-            }
-        }
+			}
+		}
 
-        public async Task<Boolean> CheckPermissionsAsync(Int32 accessAppId, params Int32[] roleIds)
-        {
-            Parameter.Validate(accessAppId);
-            Parameter.Validate(roleIds);
+		public async Task<Boolean> CheckPermissionsAsync(Int32 accessAppId, params Int32[] roleIds)
+		{
+			Parameter.Validate(accessAppId);
+			Parameter.Validate(roleIds);
 
-            return await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    #region 检查app是否为系统app
-                    {
+			return await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					#region 检查app是否为系统app
+					{
+						var result = mapper.Select<App>().Where(a => a.Id == accessAppId && a.IsSystem).ToList().Count();
+						//var sql = $@"SELECT COUNT(*) FROM App AS a WHERE a.Id=@Id AND a.IsDeleted=0 AND a.IsSystem=1";
+						//var parameters = new List<EntityParameter>
+						//{
+						//    new EntityParameter("@Id",accessAppId)
+						//};
+						//var result = mapper.FindSingleValue<Int32>(sql, parameters);
+						if (result <= 0)
+						{
+							return true;
+						}
+					}
+					#endregion
+					{
+						var result = mapper.Select<RolePower>(a => new { a.AppId }).Where(a => roleIds.Contains(a.RoleId)).ToList();
+						//var sql = $@"SELECT a.AppId FROM RolePower AS a WHERE a.RoleId IN({String.Join(",", roleIds)}) AND a.IsDeleted=0";
+						//var result = mapper.Find<RolePower>(sql);
+						return result.Any(a => a.AppId == accessAppId);
+					}
+				}
+			});
+		}
 
-                        var result = mapper.Count<App>(a => a.Id == accessAppId && a.IsSystem);
-                        //var sql = $@"SELECT COUNT(*) FROM App AS a WHERE a.Id=@Id AND a.IsDeleted=0 AND a.IsSystem=1";
-                        //var parameters = new List<EntityParameter>
-                        //{
-                        //    new EntityParameter("@Id",accessAppId)
-                        //};
-                        //var result = mapper.FindSingleValue<Int32>(sql, parameters);
-                        if (result <= 0)
-                        {
-                            return true;
-                        }
-                    }
-                    #endregion
-                    {
+		public async Task<Boolean> CheckRoleNameAsync(String name)
+		{
+			Parameter.Validate(name);
 
-                        var result = mapper.Find<RolePower>(a => roleIds.Contains(a.RoleId), a => new { a.AppId });
-                        //var sql = $@"SELECT a.AppId FROM RolePower AS a WHERE a.RoleId IN({String.Join(",", roleIds)}) AND a.IsDeleted=0";
-                        //var result = mapper.Find<RolePower>(sql);
-                        return result.Any(a => a.AppId == accessAppId);
-                    }
-                }
-            });
-        }
+			return await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					return mapper.Select<Role>().Where(a => a.Name == name).ToList().Count() > 0;
+					//var sql = $@"SELECT COUNT(*) FROM Role AS a WHERE a.Name=@name AND a.IsDeleted=0";
+					//var parameters = new List<EntityParameter>
+					//{
+					//    new EntityParameter("@name",name)
+					//};
+					//return mapper.FindSingleValue<Int32>(sql, parameters) > 0;
+				}
+			});
+		}
 
-        public async Task<Boolean> CheckRoleNameAsync(String name)
-        {
-            Parameter.Validate(name);
+		public async Task<Boolean> CheckRoleIdentityAsync(String name)
+		{
+			Parameter.Validate(name);
 
-            return await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
+			return await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					return mapper.Select<Role>().Where(a => a.RoleIdentity == name).ToList().Count() > 0;
+					//var sql = $@"SELECT COUNT(*) FROM Role AS a WHERE a.RoleIdentity=@RoleIdentity AND a.IsDeleted=0";
+					//var parameters = new List<EntityParameter>
+					//{
+					//    new EntityParameter("@RoleIdentity",name)
+					//};
+					//return mapper.FindSingleValue<Int32>(sql, parameters) > 0;
+				}
+			});
+		}
 
-                    return mapper.Count<Role>(a => a.Name == name) > 0;
-                    //var sql = $@"SELECT COUNT(*) FROM Role AS a WHERE a.Name=@name AND a.IsDeleted=0";
-                    //var parameters = new List<EntityParameter>
-                    //{
-                    //    new EntityParameter("@name",name)
-                    //};
-                    //return mapper.FindSingleValue<Int32>(sql, parameters) > 0;
-                }
-            });
-        }
+		public async Task ModifyRoleAsync(Role role)
+		{
+			Parameter.Validate(role);
 
-        public async Task<Boolean> CheckRoleIdentityAsync(String name)
-        {
-            Parameter.Validate(name);
+			await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					#region 修改角色
+					{
+						role.ModifyRoleName(role.Name);
+						var result = mapper.Modify(role, r => r.Id == role.Id);
+						if (!result)
+						{
+							throw new BusinessException("修改角色失败");
+						}
+					}
+					#endregion
+				}
+			});
+		}
 
-            return await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    return mapper.Count<Role>(a => a.RoleIdentity == name) > 0;
-                    //var sql = $@"SELECT COUNT(*) FROM Role AS a WHERE a.RoleIdentity=@RoleIdentity AND a.IsDeleted=0";
-                    //var parameters = new List<EntityParameter>
-                    //{
-                    //    new EntityParameter("@RoleIdentity",name)
-                    //};
-                    //return mapper.FindSingleValue<Int32>(sql, parameters) > 0;
-                }
-            });
-        }
+		public async Task RemoveRoleAsync(Int32 roleId)
+		{
+			Parameter.Validate(roleId);
 
-        public async Task ModifyRoleAsync(Role role)
-        {
-            Parameter.Validate(role);
+			await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					mapper.OpenTransaction();
+					try
+					{
+						//var parameters = new List<EntityParameter>
+						//{
+						//    new EntityParameter("@roleId",roleId)
+						//};
+						#region 前置条件验证
+						{
+							var result = mapper.Select<UserRole>().Where(a => a.RoleId == roleId).ToList().Count();
+							//var sql = $@"SELECT COUNT(*) FROM UserRole AS a WHERE a.RoleId=@roleId AND a.IsDeleted=0";
+							//var result = mapper.FindSingleValue<Int32>(sql, parameters);
+							if (result > 0)
+							{
+								throw new BusinessException("当前角色已绑定了账户，无法删除");
+							}
+						}
+						#endregion
 
-            await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    #region 修改角色
-                    {
-                        role.ModifyRoleName(role.Name);
-                        var result = mapper.Modify(role, r => r.Id == role.Id);
-                        if (!result)
-                        {
-                            throw new BusinessException("修改角色失败");
-                        }
-                    }
-                    #endregion
-                }
-            });
-        }
+						#region 删除角色
+						{
+							var role = new Role();
+							role.Remove();
+							var result = mapper.Modify(role, r => r.Id == roleId);
+							if (!result)
+							{
+								throw new BusinessException("删除角色失败");
+							}
+						}
+						#endregion
 
-        public async Task RemoveRoleAsync(Int32 roleId)
-        {
-            Parameter.Validate(roleId);
+						#region 移除权限
+						{
+							var rolePower = new RolePower();
+							rolePower.Remove();
+							var result = mapper.Modify(rolePower, rp => rp.RoleId == roleId);
+							if (!result)
+							{
+								throw new BusinessException("移除权限失败");
+							}
+						}
+						#endregion
 
-            await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    mapper.OpenTransaction();
-                    try
-                    {
-                        //var parameters = new List<EntityParameter>
-                        //{
-                        //    new EntityParameter("@roleId",roleId)
-                        //};
-                        #region 前置条件验证
-                        {
-                            var result = mapper.Count<UserRole>(a => a.RoleId == roleId);
-                            //var sql = $@"SELECT COUNT(*) FROM UserRole AS a WHERE a.RoleId=@roleId AND a.IsDeleted=0";
-                            //var result = mapper.FindSingleValue<Int32>(sql, parameters);
-                            if (result > 0)
-                            {
-                                throw new BusinessException("当前角色已绑定了账户，无法删除");
-                            }
-                        }
-                        #endregion
+						mapper.Commit();
+					}
+					catch (Exception)
+					{
+						mapper.Rollback();
+						throw;
+					}
+				}
+			});
+		}
 
-                        #region 删除角色
-                        {
-                            var role = new Role();
-                            role.Remove();
-                            var result = mapper.Modify(role, r => r.Id == roleId);
-                            if (!result)
-                            {
-                                throw new BusinessException("删除角色失败");
-                            }
-                        }
-                        #endregion
+		public async Task AddNewRoleAsync(Role role)
+		{
+			Parameter.Validate(role);
 
-                        #region 移除权限
-                        {
-                            var rolePower = new RolePower();
-                            rolePower.Remove();
-                            var result = mapper.Modify(rolePower, rp => rp.RoleId == roleId);
-                            if (!result)
-                            {
-                                throw new BusinessException("移除权限失败");
-                            }
-                        }
-                        #endregion
+			await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					#region 添加角色
+					{
+						mapper.Add(role);
+					}
+					#endregion
+				}
+			});
+		}
 
-                        mapper.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        mapper.Rollback();
-                        throw;
-                    }
-                }
-            });
-        }
+		public async Task AddPowerToCurrentRoleAsync(Int32 roleId, IEnumerable<Int32> powerIds)
+		{
+			Parameter.Validate(roleId);
+			Parameter.Validate(powerIds);
 
-        public async Task AddNewRoleAsync(Role role)
-        {
-            Parameter.Validate(role);
+			await Task.Run(() =>
+			{
+				if (!powerIds.Any())
+				{
+					throw new BusinessException("权限列表为空");
+				}
 
-            await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    #region 添加角色
-                    {
-                        mapper.Add(role);
-                    }
-                    #endregion
-                }
-            });
-        }
+				using (var mapper = new EntityMapper())
+				{
+					mapper.OpenTransaction();
+					try
+					{
+						#region 移除之前的角色权限
+						{
+							var rolePower = new RolePower();
+							rolePower.Remove();
+							var result = mapper.Modify(rolePower, rp => rp.RoleId == roleId);
+							if (!result)
+							{
+								throw new BusinessException("移除之前的角色权限失败");
+							}
+						}
+						#endregion
 
-        public async Task AddPowerToCurrentRoleAsync(Int32 roleId, IEnumerable<Int32> powerIds)
-        {
-            Parameter.Validate(roleId);
-            Parameter.Validate(powerIds);
+						#region 添加角色权限
+						{
+							var sqlBuilder = new StringBuilder();
+							foreach (var item in powerIds)
+							{
+								var rolePower = new RolePower(roleId, item);
+								mapper.Add(rolePower);
+							}
+						}
+						#endregion
 
-            await Task.Run(() =>
-            {
-                if (!powerIds.Any())
-                {
-                    throw new BusinessException("权限列表为空");
-                }
+						mapper.Commit();
+					}
+					catch (Exception)
+					{
+						mapper.Rollback();
+						throw;
+					}
+				}
+			});
+		}
 
-                using (var mapper = new EntityMapper())
-                {
-                    mapper.OpenTransaction();
-                    try
-                    {
-                        #region 移除之前的角色权限
-                        {
-                            var rolePower = new RolePower();
-                            rolePower.Remove();
-                            var result = mapper.Modify(rolePower, rp => rp.RoleId == roleId);
-                            if (!result)
-                            {
-                                throw new BusinessException("移除之前的角色权限失败");
-                            }
-                        }
-                        #endregion
+		public async Task AddVisitorRecord(VisitorRecord visitorRecord)
+		{
+			Parameter.Validate(visitorRecord);
 
-                        #region 添加角色权限
-                        {
-                            var sqlBuilder = new StringBuilder();
-                            foreach (var item in powerIds)
-                            {
-                                var rolePower = new RolePower(roleId, item);
-                                mapper.Add(rolePower);
-                            }
-                        }
-                        #endregion
-
-                        mapper.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        mapper.Rollback();
-                        throw;
-                    }
-                }
-            });
-        }
-
-        public async Task AddVisitorRecord(VisitorRecord visitorRecord)
-        {
-            Parameter.Validate(visitorRecord);
-
-            await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    #region 添加角色
-                    {
-                        mapper.Add(visitorRecord);
-                    }
-                    #endregion
-                }
-            });
-        }
-    }
+			await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					#region 添加角色
+					{
+						mapper.Add(visitorRecord);
+					}
+					#endregion
+				}
+			});
+		}
+	}
 }
