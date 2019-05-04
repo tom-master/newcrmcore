2286372ec75ad56b8f3f03f9d052f798582497ff
haoxiaofan <569192072@qq.com>
2019-05-04 20:08:00 +0800

    add commit

--- NewCrmCore.Domain.Services/BoundedContext/LoggerContext.cs
+++ NewCrmCore.Domain.Services/BoundedContext/LoggerContext.cs
@@ -2,56 +2,53 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using NewCrmCore.Domain.Entitys.Agent;
 using NewCrmCore.Domain.Entitys.System;
 using NewCrmCore.Domain.Services.Interface;
-using NewLibCore.Data.SQL.CombineCondition;
-using NewLibCore.Data.SQL.CombineCondition.ConcreteCombineCondition;
 using NewLibCore.Data.SQL.Mapper;
 using NewLibCore.Validate;
 
 namespace NewCrmCore.Domain.Services.BoundedContext
 {
-    public class LoggerContext : ILoggerContext
-    {
-        public async Task AddLoggerAsync(Log log)
-        {
-            Parameter.Validate(log);
-            await Task.Run(() =>
-            {
-                using (var mapper = new EntityMapper())
-                {
-                    mapper.Add(log);
-                }
-            });
-        }
+	public class LoggerContext : ILoggerContext
+	{
+		public async Task AddLoggerAsync(Log log)
+		{
+			Parameter.Validate(log);
+			await Task.Run(() =>
+			{
+				using (var mapper = new EntityMapper())
+				{
+					mapper.Add(log);
+				}
+			});
+		}
 
-        public List<Log> GetLogs(String userName, Int32 logLevel, Int32 pageIndex, Int32 pageSize, out Int32 totalCount)
-        {
-            Parameter.Validate(userName, true);
-            Parameter.Validate(logLevel);
+		public List<Log> GetLogs(String userName, Int32 logLevel, Int32 pageIndex, Int32 pageSize, out Int32 totalCount)
+		{
+			Parameter.Validate(userName, true);
+			Parameter.Validate(logLevel);
 
-            using (var mapper = new EntityMapper())
-            {
-                var where = new StringBuilder();
-                var parameters = new List<EntityParameter>();
+			using (var mapper = new EntityMapper())
+			{
+				var where = new StringBuilder();
+				var parameters = new List<EntityParameter>();
 
-                if (!String.IsNullOrEmpty(userName))
-                {
-                    parameters.Add(new EntityParameter("@name", userName));
-                    where.Append($@" AND a1.Name LIKE CONCAT('%',@name,'%') ");
-                }
+				if (!String.IsNullOrEmpty(userName))
+				{
+					parameters.Add(new EntityParameter("@name", userName));
+					where.Append($@" AND a1.Name LIKE CONCAT('%',@name,'%') ");
+				}
 
-                #region totalCount 
-                {
-                    var sql = $@"SELECT COUNT(*) FROM Log AS a LEFT JOIN User AS a1 ON a.UserId=a1.Id WHERE 1=1 {where}";
-                    totalCount = mapper.ComplexSqlExecute<Int32>(sql, parameters);
-                }
-                #endregion
+				#region totalCount 
+				{
+					var sql = $@"SELECT COUNT(*) FROM Log AS a LEFT JOIN User AS a1 ON a.UserId=a1.Id WHERE 1=1 {where}";
+					totalCount = mapper.ExecuteSql<Int32>(sql, parameters);
+				}
+				#endregion
 
-                #region sql 
-                {
-                    var sql = $@"SELECT
+				#region sql 
+				{
+					var sql = $@"SELECT
                                 a.LogLevelEnum,
                                 a.Controller,
                                 a.Action,
@@ -61,10 +58,10 @@ namespace NewCrmCore.Domain.Services.BoundedContext
                                 FROM Log AS a 
                                 LEFT JOIN User AS a1 ON a.UserId=a1.Id
                                 WHERE 1=1 {where} ORDER BY a.AddTime DESC LIMIT {pageSize * (pageIndex - 1)},{pageSize}";
-                    return mapper.ComplexSqlExecute<List<Log>>(sql, parameters);
-                }
-                #endregion
-            }
-        }
-    }
+					return mapper.ExecuteSql<List<Log>>(sql, parameters);
+				}
+				#endregion
+			}
+		}
+	}
 }
