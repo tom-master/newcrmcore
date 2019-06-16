4077a83dfa78626043da42e961b6ac189cfb08d6
haoxiaofan <569192072@qq.com>
2019-06-16 09:23:09 +0800

    add commit

--- NewCrmCore.Domain.Services/BoundedContext/WallpaperContext.cs
+++ NewCrmCore.Domain.Services/BoundedContext/WallpaperContext.cs
@@ -11,266 +11,266 @@ using NewLibCore.Validate;
 
 namespace NewCrmCore.Domain.Services.BoundedContext
 {
-	public class WallpaperContext : IWallpaperContext
-	{
-		public async Task<Tuple<Int32, String>> AddWallpaperAsync(Wallpaper wallpaper)
-		{
-			Parameter.Validate(wallpaper);
-			return await Task.Run(() =>
-			 {
-				 using (var mapper = new EntityMapper())
-				 {
-					 #region 前置条件验证
-					 {
-						 //  var sql = $@"SELECT COUNT(*) FROM Wallpaper AS a WHERE a.UserId=@UserId AND a.IsDeleted=0";
-						 //  var parameters = new List<EntityParameter>
-						 //  {
-						 //     new EntityParameter("@UserId",wallpaper.UserId)
-						 //  };
-						 //  var result = dataStore.FindSingleValue<Int32>(sql, parameters);
-						 var result = mapper.Select<Wallpaper>().Where(w => w.UserId == wallpaper.UserId).Count();
-						 if (result > 6)
-						 {
-							 throw new BusinessException("最多只能上传6张图片");
-						 }
-					 }
-					 #endregion
+    public class WallpaperContext : IWallpaperContext
+    {
+        public async Task<Tuple<Int32, String>> AddWallpaperAsync(Wallpaper wallpaper)
+        {
+            Parameter.Validate(wallpaper);
+            return await Task.Run(() =>
+             {
+                 using (var mapper = new EntityMapper())
+                 {
+                     #region 前置条件验证
+                     {
+                         //  var sql = $@"SELECT COUNT(*) FROM Wallpaper AS a WHERE a.UserId=@UserId AND a.IsDeleted=0";
+                         //  var parameters = new List<EntityParameter>
+                         //  {
+                         //     new EntityParameter("@UserId",wallpaper.UserId)
+                         //  };
+                         //  var result = dataStore.FindSingleValue<Int32>(sql, parameters);
+                         var result = mapper.Select<Wallpaper>().Where(w => w.UserId == wallpaper.UserId).Count();
+                         if (result > 6)
+                         {
+                             throw new BusinessException("最多只能上传6张图片");
+                         }
+                     }
+                     #endregion
 
-					 #region 插入壁纸
-					 {
-						 wallpaper = mapper.Add(wallpaper);
-						 return new Tuple<Int32, String>(wallpaper.Id, wallpaper.Url);
-					 }
-					 #endregion
+                     #region 插入壁纸
+                     {
+                         wallpaper = mapper.Add(wallpaper);
+                         return new Tuple<Int32, String>(wallpaper.Id, wallpaper.Url);
+                     }
+                     #endregion
 
-					 #region 获取返回值
-					 {
-						 //  var sql = $@"SELECT a.Id,a.Url FROM Wallpaper AS a WHERE a.Id=@Id AND a.IsDeleted=0";
-						 //  var parameters = new List<EntityParameter>
-						 //  {
-						 //     new EntityParameter("@Id",newWallpaperId)
-						 //  };
-						 //  var result = dataStore.FindOne<Wallpaper>(sql, parameters);
-						 //  if (result != null)
-						 //  {
-						 //      return new Tuple<Int32, String>(result.Id, result.Url);
-						 //  }
-						 //  return null;
-					 }
-					 #endregion
-				 }
-			 });
-		}
+                     #region 获取返回值
+                     {
+                         //  var sql = $@"SELECT a.Id,a.Url FROM Wallpaper AS a WHERE a.Id=@Id AND a.IsDeleted=0";
+                         //  var parameters = new List<EntityParameter>
+                         //  {
+                         //     new EntityParameter("@Id",newWallpaperId)
+                         //  };
+                         //  var result = dataStore.FindOne<Wallpaper>(sql, parameters);
+                         //  if (result != null)
+                         //  {
+                         //      return new Tuple<Int32, String>(result.Id, result.Url);
+                         //  }
+                         //  return null;
+                     }
+                     #endregion
+                 }
+             });
+        }
 
-		public async Task<Wallpaper> GetUploadWallpaperAsync(String md5)
-		{
-			Parameter.Validate(md5);
+        public async Task<Wallpaper> GetUploadWallpaperAsync(String md5)
+        {
+            Parameter.Validate(md5);
 
-			return await Task.Run(() =>
-			{
-				using (var mapper = new EntityMapper())
-				{
-					return mapper.Select<Wallpaper>(a => new
-					{
-						a.UserId,
-						a.Height,
-						a.Id,
-						a.Md5,
-						a.ShortUrl,
-						a.Source,
-						a.Title,
-						a.Url,
-						a.Width
-					}).Where(a => a.Md5 == md5).FirstOrDefault();
+            return await Task.Run(() =>
+            {
+                using (var mapper = new EntityMapper())
+                {
+                    return mapper.Select<Wallpaper>(a => new
+                    {
+                        a.UserId,
+                        a.Height,
+                        a.Id,
+                        a.Md5,
+                        a.ShortUrl,
+                        a.Source,
+                        a.Title,
+                        a.Url,
+                        a.Width
+                    }).Where(a => a.Md5 == md5).FirstOrDefault();
 
-					// var sql = $@"SELECT
-					//         a.UserId,
-					//         a.Height,
-					//         a.Id,
-					//         a.Md5,
-					//         a.ShortUrl,
-					//         a.Source,
-					//         a.Title,
-					//         a.Url,
-					//         a.Width
-					//         FROM Wallpaper AS a WHERE a.Md5=@Md5 AND a.IsDeleted=0";
-					// var parameters = new List<EntityParameter>
-					// {
-					//     new EntityParameter("@Md5",md5)
-					// };
-					// return dataStore.FindOne<Wallpaper>(sql, parameters);
-				}
-			});
-		}
+                    // var sql = $@"SELECT
+                    //         a.UserId,
+                    //         a.Height,
+                    //         a.Id,
+                    //         a.Md5,
+                    //         a.ShortUrl,
+                    //         a.Source,
+                    //         a.Title,
+                    //         a.Url,
+                    //         a.Width
+                    //         FROM Wallpaper AS a WHERE a.Md5=@Md5 AND a.IsDeleted=0";
+                    // var parameters = new List<EntityParameter>
+                    // {
+                    //     new EntityParameter("@Md5",md5)
+                    // };
+                    // return dataStore.FindOne<Wallpaper>(sql, parameters);
+                }
+            });
+        }
 
-		public async Task<List<Wallpaper>> GetUploadWallpaperAsync(Int32 userId)
-		{
-			Parameter.Validate(userId);
+        public async Task<List<Wallpaper>> GetUploadWallpaperAsync(Int32 userId)
+        {
+            Parameter.Validate(userId);
 
-			return await Task.Run(() =>
-			{
-				using (var mapper = new EntityMapper())
-				{
-					return mapper.Select<Wallpaper>(a => new
-					{
-						a.UserId,
-						a.Height,
-						a.Id,
-						a.Md5,
-						a.ShortUrl,
-						a.Source,
-						a.Title,
-						a.Url,
-						a.Width
-					}).Where(a => a.UserId == userId && a.Source != WallpaperSource.System).ToList();
+            return await Task.Run(() =>
+            {
+                using (var mapper = new EntityMapper())
+                {
+                    return mapper.Select<Wallpaper>(a => new
+                    {
+                        a.UserId,
+                        a.Height,
+                        a.Id,
+                        a.Md5,
+                        a.ShortUrl,
+                        a.Source,
+                        a.Title,
+                        a.Url,
+                        a.Width
+                    }).Where(a => a.UserId == userId && a.Source != WallpaperSource.System).ToList();
 
-					// var sql = $@"SELECT
-					//         a.UserId,
-					//         a.Height,
-					//         a.Id,
-					//         a.Md5,
-					//         a.ShortUrl,
-					//         a.Source,
-					//         a.Title,
-					//         a.Url,
-					//         a.Width
-					//         FROM Wallpaper AS a WHERE a.UserId=@UserId AND a.Source<>@Source AND a.IsDeleted=0";
-					// var parameters = new List<EntityParameter>
-					// {
-					//     new EntityParameter("@UserId",userId),
-					//     new EntityParameter("@Source", WallpaperSource.System.ToInt32())
-					// };
-					// return dataStore.Find<Wallpaper>(sql, parameters);
-				}
-			});
-		}
+                    // var sql = $@"SELECT
+                    //         a.UserId,
+                    //         a.Height,
+                    //         a.Id,
+                    //         a.Md5,
+                    //         a.ShortUrl,
+                    //         a.Source,
+                    //         a.Title,
+                    //         a.Url,
+                    //         a.Width
+                    //         FROM Wallpaper AS a WHERE a.UserId=@UserId AND a.Source<>@Source AND a.IsDeleted=0";
+                    // var parameters = new List<EntityParameter>
+                    // {
+                    //     new EntityParameter("@UserId",userId),
+                    //     new EntityParameter("@Source", WallpaperSource.System.ToInt32())
+                    // };
+                    // return dataStore.Find<Wallpaper>(sql, parameters);
+                }
+            });
+        }
 
-		public async Task<List<Wallpaper>> GetWallpapersAsync()
-		{
-			return await Task.Run(() =>
-			{
-				using (var mapper = new EntityMapper())
-				{
-					return mapper.Select<Wallpaper>(a => new
-					{
-						a.UserId,
-						a.Height,
-						a.Id,
-						a.Md5,
-						a.ShortUrl,
-						a.Source,
-						a.Title,
-						a.Url,
-						a.Width
-					}).Where(a => a.Source == WallpaperSource.System).ToList();
+        public async Task<List<Wallpaper>> GetWallpapersAsync()
+        {
+            return await Task.Run(() =>
+            {
+                using (var mapper = new EntityMapper())
+                {
+                    return mapper.Select<Wallpaper>(a => new
+                    {
+                        a.UserId,
+                        a.Height,
+                        a.Id,
+                        a.Md5,
+                        a.ShortUrl,
+                        a.Source,
+                        a.Title,
+                        a.Url,
+                        a.Width
+                    }).Where(a => a.Source == WallpaperSource.System).ToList();
 
-					// var sql = $@"SELECT
-					//         a.UserId,
-					//         a.Height,
-					//         a.Id,
-					//         a.Md5,
-					//         a.ShortUrl,
-					//         a.Source,
-					//         a.Title,
-					//         a.Url,
-					//         a.Width
-					//         FROM Wallpaper AS a WHERE a.Source=@Source AND a.IsDeleted=0";
-					// var parameters = new List<EntityParameter>
-					// {
-					//     new EntityParameter("@Source", WallpaperSource.System.ToInt32())
-					// };
-					// return dataStore.Find<Wallpaper>(sql, parameters);
-				}
-			});
-		}
+                    // var sql = $@"SELECT
+                    //         a.UserId,
+                    //         a.Height,
+                    //         a.Id,
+                    //         a.Md5,
+                    //         a.ShortUrl,
+                    //         a.Source,
+                    //         a.Title,
+                    //         a.Url,
+                    //         a.Width
+                    //         FROM Wallpaper AS a WHERE a.Source=@Source AND a.IsDeleted=0";
+                    // var parameters = new List<EntityParameter>
+                    // {
+                    //     new EntityParameter("@Source", WallpaperSource.System.ToInt32())
+                    // };
+                    // return dataStore.Find<Wallpaper>(sql, parameters);
+                }
+            });
+        }
 
-		public async Task ModifyWallpaperModeAsync(Int32 userId, String newMode)
-		{
-			Parameter.Validate(userId);
-			Parameter.Validate(newMode);
+        public async Task ModifyWallpaperModeAsync(Int32 userId, String newMode)
+        {
+            Parameter.Validate(userId);
+            Parameter.Validate(newMode);
 
-			await Task.Run(() =>
-			{
-				if (Enum.TryParse(newMode, true, out WallpaperMode wallpaperMode))
-				{
-					using (var mapper = new EntityMapper())
-					{
-						var config = new Config();
-						config.ModeTo(wallpaperMode);
-						var result = mapper.Modify(config, conf => conf.UserId == userId);
-						if (!result)
-						{
-							throw new BusinessException("修改壁纸显示失败");
-						}
-					}
-				}
-				else
-				{
-					throw new BusinessException($"无法识别的壁纸显示模式:{newMode}");
-				}
-			});
-		}
+            await Task.Run(() =>
+            {
+                if (Enum.TryParse(newMode, true, out WallpaperMode wallpaperMode))
+                {
+                    using (var mapper = new EntityMapper())
+                    {
+                        var config = new Config();
+                        config.ModeTo(wallpaperMode);
+                        var result = mapper.Modify(config, conf => conf.UserId == userId);
+                        if (!result)
+                        {
+                            throw new BusinessException("修改壁纸显示失败");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new BusinessException($"无法识别的壁纸显示模式:{newMode}");
+                }
+            });
+        }
 
-		public async Task ModifyWallpaperAsync(Int32 userId, Int32 newWallpaperId)
-		{
-			Parameter.Validate(userId);
-			Parameter.Validate(newWallpaperId);
+        public async Task ModifyWallpaperAsync(Int32 userId, Int32 newWallpaperId)
+        {
+            Parameter.Validate(userId);
+            Parameter.Validate(newWallpaperId);
 
-			await Task.Run(() =>
-			{
-				using (var mapper = new EntityMapper())
-				{
-					var config = new Config();
-					config.NotFromBing().ModifyWallpaperId(newWallpaperId);
-					var result = mapper.Modify(config, conf => conf.UserId == userId);
-					if (!result)
-					{
-						throw new BusinessException("修改壁纸失败");
-					}
-				}
-			});
-		}
+            await Task.Run(() =>
+            {
+                using (var mapper = new EntityMapper())
+                {
+                    var config = new Config();
+                    config.NotFromBing().ModifyWallpaperId(newWallpaperId);
+                    var result = mapper.Modify(config, conf => conf.UserId == userId);
+                    if (!result)
+                    {
+                        throw new BusinessException("修改壁纸失败");
+                    }
+                }
+            });
+        }
 
-		public async Task RemoveWallpaperAsync(Int32 userId, Int32 wallpaperId)
-		{
-			Parameter.Validate(userId);
-			Parameter.Validate(wallpaperId);
+        public async Task RemoveWallpaperAsync(Int32 userId, Int32 wallpaperId)
+        {
+            Parameter.Validate(userId);
+            Parameter.Validate(wallpaperId);
 
-			await Task.Run(() =>
-			{
-				using (var mapper = new EntityMapper())
-				{
-					#region 前置条件验证
-					{
-						var result = mapper.Select<Wallpaper>().Where(a => a.UserId == userId && a.Id == wallpaperId).Count();
+            await Task.Run(() =>
+            {
+                using (var mapper = new EntityMapper())
+                {
+                    #region 前置条件验证
+                    {
+                        var result = mapper.Select<Config>().Where(a => a.UserId == userId && a.WallpaperId == wallpaperId).Count();
 
-						// var sql = $@"SELECT COUNT(*) FROM Config AS a WHERE a.UserId=@UserId AND a.WallpaperId=@WallpaperId AND a.IsDeleted=0";
-						// var parameters = new List<EntityParameter>
-						// {
-						//     new EntityParameter("@WallpaperId",wallpaperId),
-						//     new EntityParameter("@UserId",userId)
-						// };
-						// var result = dataStore.FindSingleValue<Int32>(sql, parameters);
-						if (result > 0)
-						{
-							throw new BusinessException("当前壁纸正在使用中，不能删除");
-						}
-					}
-					#endregion
+                        // var sql = $@"SELECT COUNT(*) FROM Config AS a WHERE a.UserId=@UserId AND a.WallpaperId=@WallpaperId AND a.IsDeleted=0";
+                        // var parameters = new List<EntityParameter>
+                        // {
+                        //     new EntityParameter("@WallpaperId",wallpaperId),
+                        //     new EntityParameter("@UserId",userId)
+                        // };
+                        // var result = dataStore.FindSingleValue<Int32>(sql, parameters);
+                        if (result > 0)
+                        {
+                            throw new BusinessException("当前壁纸正在使用中，不能删除");
+                        }
+                    }
+                    #endregion
 
-					#region 移除壁纸
-					{
-						var wallpaper = new Wallpaper();
-						wallpaper.Remove();
-						var result = mapper.Modify(wallpaper, wa => wa.Id == wallpaperId && wa.UserId == userId);
-						if (!result)
-						{
-							throw new BusinessException("移除壁纸失败");
-						}
-					}
-					#endregion
-				}
-			});
-		}
-	}
+                    #region 移除壁纸
+                    {
+                        var wallpaper = new Wallpaper();
+                        wallpaper.Remove();
+                        var result = mapper.Modify(wallpaper, wa => wa.Id == wallpaperId && wa.UserId == userId);
+                        if (!result)
+                        {
+                            throw new BusinessException("移除壁纸失败");
+                        }
+                    }
+                    #endregion
+                }
+            });
+        }
+    }
 }
