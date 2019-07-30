e11581cff880c202c28b0bf020e0a6446c65c947
haoxiaofan <569192072@qq.com>
2019-07-30 20:15:47 +0800

    add commit

--- NewCrmCore.Web/wwwroot/js/lobibox/notifications.js
+++ NewCrmCore.Web/wwwroot/js/lobibox/notifications.js
@@ -354,7 +354,7 @@ var Lobibox = Lobibox || {};
         title: true,                // Title of notification. If you do not include the title in options it will automatically takes its value 
         //from Lobibox.notify.OPTIONS object depending of the type of the notifications or set custom string. Set this false to disable title
         size: 'normal',             // normal, mini, large
-        soundPath: 'sounds/',   // The folder path where sounds are located
+        soundPath: '/js/sounds/',   // The folder path where sounds are located
         soundExt: '.ogg',           // Default extension for all sounds
         showClass: 'fadeInDown',    // Show animation class.
         hideClass: 'zoomOut',       // Hide animation class.
