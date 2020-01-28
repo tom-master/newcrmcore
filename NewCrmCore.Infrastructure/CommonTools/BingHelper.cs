29c4718a16a0a1aa284bac243f1ae71ef15aa74d
xiaofan <569192072@qq.com>
2020-01-28 10:09:07 +0800

    add commit

--- NewCrmCore.Infrastructure/CommonTools/BingHelper.cs
+++ NewCrmCore.Infrastructure/CommonTools/BingHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace NewCrmCore.Infrastructure.CommonTools
 {
