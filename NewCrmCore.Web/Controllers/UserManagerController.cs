b19954f47c88d5b6f8046ffc94413646a768094d
xiaofan <569192072@qq.com>
2020-02-27 10:30:06 +0800

    add commit

--- NewCrmCore.Web/Controllers/UserManagerController.cs
+++ NewCrmCore.Web/Controllers/UserManagerController.cs
@@ -66,12 +66,12 @@ namespace NewCrmCore.Web.Controllers
             Parameter.Validate(userId);
             #endregion
 
-            var response = new ResponseModel<String>();
             await _userServices.RemoveUserAsync(userId);
-            response.IsSuccess = true;
-            response.Message = "移除账户成功";
-
-            return Json(response);
+            return Json(new ResponseModel<String>
+            {
+                IsSuccess = true,
+                Message = "移除账户成功"
+            });
         }
 
         #endregion
