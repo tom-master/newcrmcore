59ec981fe1b999950bda3c1f217237ff75115f02
xiaofan <569192072@qq.com>
2020-05-05 10:50:59 +0800

    add commit

--- NewCrmCore.Api/Controllers/LoggerController.cs
+++ NewCrmCore.Api/Controllers/LoggerController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NewCrmCore.Application.Services.Interface;
 using NewCrmCore.Dto;
