f407015ad846383f403cea2bb3407d77726693af
xiaofan <569192072@qq.com>
2020-05-14 10:59:22 +0800

    add commit

--- NewCrmCore.Api/Controllers/UserManagerController.cs
+++ NewCrmCore.Api/Controllers/UserManagerController.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NewCrmCore.Application.Services.Interface;
 using NewCrmCore.Dto;
 using NewLibCore.Validate;
 
-namespace NewCrmCore.Web.Controllers
+namespace NewCrmCore.Api.Controllers
 {
     public class UserManagerController : NewCrmController
     {
