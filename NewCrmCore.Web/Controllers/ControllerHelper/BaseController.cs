b8399992331abf5034ae13d713d0e3734a71fc2e
xiaofan <569192072@qq.com>
2020-06-02 10:09:18 +0800

    add commit

--- NewCrmCore.Web/Controllers/ControllerHelper/BaseController.cs
+++ NewCrmCore.Web/Controllers/ControllerHelper/BaseController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NewCrmCore.Dto;
+using NewCrmCore.Infrastructure.CommonTools;
 using Newtonsoft.Json;
 using Nito.AsyncEx;
+using static NewCrmCore.Infrastructure.CommonTools.CacheKey;
 
 namespace NewCrmCore.Web.Controllers
 {
@@ -26,9 +29,21 @@ namespace NewCrmCore.Web.Controllers
             }
         }
 
-        internal void InternalLogout()
+        protected void InternalLogout()
         {
             Response.Cookies.Append("User", "", new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
+        }
+
+        protected async Task<String> CreateUniqueTokenAsync(Int32 userId)
+        {
+            var milliseconds = (DateTime.Now - (new DateTime(1970, 1, 1))).TotalMilliseconds.ToString();
+            await CacheHelper.GetOrSetCacheAsync(new GlobalUniqueTokenCacheKey(userId.ToString()), async () => await Task.Run(() => milliseconds));
+            return milliseconds;
+        }
+
+        protected async Task RemoveUniqueTokenAsync(Int32 userId)
+        {
+            await CacheHelper.RemoveKeyWhenModify(new GlobalUniqueTokenCacheKey(userId.ToString()));
         }
     }
 }
